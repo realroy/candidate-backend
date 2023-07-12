4fd061dd4935a27033419084bb2022b4d276372a
realroy <roiboon.cc@gmail.com>
2023-07-12 23:48:52 +0700

    feat(module): add controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,37 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppointmentsCommentsController } from './appointments-comments.controller';
+import { AppointmentsSaveController } from './appointments-save.controller';
+import { AppointmentsController } from './appointments.controller';
+
 import { PrismaLib } from './prisma.lib';
+
+import { AppService } from './app.service';
+import { CreateCommentService } from './create-comment.service';
+import { GetAppointmentsForCandidateService } from './get-appointments-for-candidate.service';
+import { SaveAppointmentByCandidateService } from './save-appointment-by-candidate.service';
+import { GetCommentByAppointmentIdService } from './get-comment-by-appointment-id.service';
+import { GetAppointmentForCandidateService } from './get-appointment-for-candidate.service';
+import { UpdateAppointmentByAdminService } from './update-appointment-by-admin.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaLib],
+  controllers: [
+    AppController,
+    AppointmentsCommentsController,
+    AppointmentsSaveController,
+    AppointmentsController,
+  ],
+  providers: [
+    AppService,
+    CreateCommentService,
+    GetAppointmentForCandidateService,
+    GetAppointmentsForCandidateService,
+    GetCommentByAppointmentIdService,
+    PrismaLib,
+    SaveAppointmentByCandidateService,
+    UpdateAppointmentByAdminService,
+  ],
 })
 export class AppModule {}
