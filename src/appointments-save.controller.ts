093b618872ce9de340c08f68c746ee483ef726b3
realroy <roiboon.cc@gmail.com>
2023-07-13 00:34:24 +0700

    refactor(controller): appointment save

--- src/appointments-save.controller.ts
+++ src/appointments-save.controller.ts
@@ -1,17 +1,29 @@
 import { Controller, Param, Post } from '@nestjs/common';
+import { IsNumber } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 import { SaveAppointmentByCandidateService } from './save-appointment-by-candidate.service';
 
-@Controller('appointments/:id')
+import type { CandidateAppointment } from '@prisma/client';
+
+import { toNumber } from './app.transformer';
+
+export class CreateAppointmentSaveParams {
+  @IsNumber()
+  @Transform(toNumber)
+  id: CandidateAppointment['id'];
+}
+
+@Controller('appointments/:id/save')
 export class AppointmentsSaveController {
   constructor(
     private readonly saveAppointmentByCandidateService: SaveAppointmentByCandidateService,
   ) {}
 
-  @Post('save')
-  create(@Param('id') id: string) {
+  @Post()
+  create(@Param() params: CreateAppointmentSaveParams) {
     return this.saveAppointmentByCandidateService.call({
-      appointmentId: +id,
+      appointmentId: params.id,
       candidateId: 1,
     });
   }
