c271be327b97924c223a0d48db77643689d48c7e
realroy <roiboon.cc@gmail.com>
2023-07-13 00:41:40 +0700

    refactor(controller): appointment

--- src/appointments.controller.ts
+++ src/appointments.controller.ts
@@ -1,20 +1,47 @@
 import { Controller, Get, Param, Patch, Query } from '@nestjs/common';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 import { GetAppointmentsForCandidateService } from './get-appointments-for-candidate.service';
 import { GetAppointmentForCandidateService } from './get-appointment-for-candidate.service';
 import { UpdateAppointmentByAdminService } from './update-appointment-by-admin.service';
 
+import { toNumber } from './app.transformer';
+
 import type { Appointment } from '@prisma/client';
 
-export class UpdateAppointmentsBodyDto {
+export class UpdateAppointmentDTO {
+  @IsString()
   name: Appointment['name'];
+
+  @IsString()
   description: Appointment['description'];
+
+  @IsString()
   status: Appointment['status'];
 }
 
-export class IndexAppointmentsQueryDto {
+export class GetAppointmentsQuery {
+  @IsNumber()
+  @Transform(toNumber)
   limit: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Transform(toNumber)
   cursor?: Appointment['id'];
+}
+
+export class GetAppointmentParams {
+  @IsNumber()
+  @Transform(toNumber)
+  id: Appointment['id'];
+}
+
+export class UpdateAppointmentParams {
+  @IsNumber()
+  @Transform(toNumber)
+  id: Appointment['id'];
 }
 
 @Controller('appointments')
@@ -26,23 +53,28 @@ export class AppointmentsController {
   ) {}
 
   @Get()
-  index(@Query() query: IndexAppointmentsQueryDto) {
+  getAppointments(@Query() query: GetAppointmentsQuery) {
     return this.getAppointmentsForCandidateService.call({
       candidateId: 1,
       ...query,
     });
   }
 
-  @Get('/:id')
-  show(@Param('id') id: string) {
-    return this.getAppointmentForCandidateService.call({ appointmentId: +id });
+  @Get(':id')
+  getAppointment(@Param() params: GetAppointmentParams) {
+    return this.getAppointmentForCandidateService.call({
+      appointmentId: params.id,
+    });
   }
 
-  @Patch('/:id')
-  update(@Param('id') id: string, body: UpdateAppointmentsBodyDto) {
+  @Patch(':id')
+  updateAppointment(
+    @Param() params: UpdateAppointmentParams,
+    body: UpdateAppointmentDTO,
+  ) {
     return this.updateAppointmentByAdminService.call({
       appointment: {
-        id: +id,
+        id: params.id,
         ...body,
       },
     });
