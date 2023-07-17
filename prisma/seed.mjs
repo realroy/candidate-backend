832e0408c98d1d4d319092a0c383dd8e5d6a3e2d
realroy <roiboon.cc@gmail.com>
2023-07-17 18:46:09 +0700

    fix: segmentation fault during migrate

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -14,54 +14,49 @@ async function main() {
     },
   });
 
-  await Promise.all([
-    prisma.candidate.upsert({
-      where: {
-        name: 'แบทแมน',
-      },
-      update: {},
-      create: {
-        name: 'แบทแมน',
-      },
-    }),
-    prisma.candidate.upsert({
-      where: {
-        name: 'แคทวูแมน',
-      },
-      update: {},
-      create: {
-        name: 'แคทวูแมน',
-      },
-    }),
-    ...Array.from({ length: 21 }, (_, i) => i).map((i) =>
-      prisma.appointment.upsert({
-        where: {
-          name: `นัดสัมภาษณ์งาน #${i + 1}`,
-        },
-        update: {},
-        create: {
-          name: `นัดสัมภาษณ์งาน #${i + 1}`,
-          description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-          creatorId: admin.id,
-        },
-      }),
-    ),
-  ]);
+  const candidate1 = await prisma.candidate.upsert({
+    where: {
+      name: 'แบทแมน',
+    },
+    update: {},
+    create: {
+      name: 'แบทแมน',
+    },
+  });
 
-  const [candidate1, candidate2, appointments] = await Promise.all([
-    prisma.candidate.findFirst({ where: { name: 'แบทแมน' } }),
-    prisma.candidate.findFirst({ where: { name: 'แคทวูแมน' } }),
-    prisma.appointment.findMany({
+  const candidate2 = await prisma.candidate.upsert({
+    where: {
+      name: 'แคทวูแมน',
+    },
+    update: {},
+    create: {
+      name: 'แคทวูแมน',
+    },
+  });
+
+  const appointmentIds = [];
+
+  for (const i in Array.from({ length: 21 }, (_, i) => i)) {
+    const appointment = await prisma.appointment.upsert({
       select: {
         id: true,
       },
-    }),
-  ]);
+      where: {
+        name: `นัดสัมภาษณ์งาน #${i + 1}`,
+      },
+      update: {},
+      create: {
+        name: `นัดสัมภาษณ์งาน #${i + 1}`,
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+        creatorId: admin.id,
+      },
+    });
 
-  // console.log({ candidate1, candidate2, appointments });
+    appointmentIds.push(appointment.id);
+  }
 
-  for (const i in appointments) {
-    const appointmentId = appointments[i].id;
+  for (const i in appointmentIds.keys()) {
+    const appointmentId = appointments[i];
 
     const commentCount = await prisma.comment.count({
       where: { appointmentId },
@@ -95,6 +90,7 @@ async function main() {
     });
   }
 }
+
 main()
   .then(async () => {
     await prisma.$disconnect();
--- scripts/start-prod.sh
+++ scripts/start-prod.sh
@@ -2,5 +2,5 @@
 
 ./wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
 ./node_modules/.bin/prisma migrate deploy
-npm prisma db seed
+./node_modules/.bin/prisma db seed
 node ./dist/main.js
