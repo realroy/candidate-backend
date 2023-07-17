81a4fca4d4d20c741f06ad1b0c067bfbbc54487b
realroy <roiboon.cc@gmail.com>
2023-07-17 11:05:17 +0700

    feat(spec): add generate token helper

---
+++ src/tests/helpers/generate-token.ts
@@ -0,0 +1,15 @@
+import * as jwt from 'jsonwebtoken';
+
+import type { CurrentUser } from '../../../src/app.type';
+
+export function generateToken(currentUser: CurrentUser) {
+  const token = jwt.sign(
+    { sub: +currentUser.id, role: currentUser.role },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '15m',
+    },
+  );
+
+  return token;
+}
