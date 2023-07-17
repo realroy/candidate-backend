79647f423bc98813c622e0ee57ff89aa1ac1456a
realroy <roiboon.cc@gmail.com>
2023-07-17 11:05:42 +0700

    fix(spec): appointment-save e2e

--- test/appointments-save.e2e-spec.ts
+++ test/appointments-save.e2e-spec.ts
@@ -4,12 +4,17 @@ import request from 'supertest';
 
 import { AppModule } from './../src/app.module';
 import prisma from './../src/tests/helpers/prisma';
+import { generateToken } from './../src/tests/helpers/generate-token';
 
-import type { Admin } from '@prisma/client';
+import type { Admin, Appointment, Candidate } from '@prisma/client';
 
 describe('AppointmentsSaveController (e2e)', () => {
   let app: INestApplication;
   let admin: Admin;
+  let adminToken: string;
+  let candidate1: Candidate;
+  let candidate1Token: string;
+  let appointment: Appointment;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -21,26 +26,58 @@ describe('AppointmentsSaveController (e2e)', () => {
   });
 
   beforeEach(async () => {
-    admin = await prisma.admin.create({
-      data: {
-        name: 'admin',
-        email: 'foo@bar.com',
-      },
-    });
+    const responses = await Promise.all([
+      prisma.admin.create({
+        data: {
+          name: 'admin',
+          email: 'foo@bar.com',
+        },
+      }),
+      prisma.candidate.create({
+        data: {
+          name: 'candidate1',
+        },
+      }),
+      prisma.appointment.create({
+        data: {
+          name: 'Appoitment#1',
+          creatorId: admin.id,
+        },
+      }),
+    ]);
+
+    admin = responses[0];
+    adminToken = generateToken({ id: admin.id, role: 'ADMIN' });
+
+    candidate1 = responses[1];
+    candidate1Token = generateToken({ id: candidate1.id, role: 'CANDIDATE' });
+
+    appointment = responses[2];
   });
 
-  it('/appointments/:id/save (POST)', async () => {
-    const appointment = await prisma.appointment.create({
-      data: {
-        name: 'Appoitment#1',
-        creatorId: admin.id,
-      },
+  describe('/appointments/:id/save (POST)', async () => {
+    it('return 404 when cannot find appointment with given id', async () => {
+      const { statusCode } = await request(app.getHttpServer())
+        .post(`/appointments/${-1}/save`)
+        .set('Authorization', `Bearer ${candidate1Token}`);
+
+      expect(statusCode).toBe(404);
     });
 
-    const { statusCode } = await request(app.getHttpServer()).post(
-      `/appointments/${appointment.id}/save`,
-    );
+    it('return 201 when candidate save appointment', async () => {
+      const { statusCode } = await request(app.getHttpServer())
+        .post(`/appointments/${appointment.id}/save`)
+        .set('Authorization', `Bearer ${candidate1Token}`);
 
-    expect(statusCode).toBe(201);
+      expect(statusCode).toBe(201);
+    });
+
+    it('return 401 when admin save appointment', async () => {
+      const { statusCode } = await request(app.getHttpServer())
+        .post(`/appointments/${appointment.id}/save`)
+        .set('Authorization', `Bearer ${adminToken}`);
+
+      expect(statusCode).toBe(401);
+    });
   });
 });
